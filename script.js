90dbf1d22945367738ccf152207a4d00e0e3ae0b
faiza203 <60883181+faiza203@users.noreply.github.com>
2020-04-25 14:41:41 +0500

    insert script

---
+++ script.js
@@ -0,0 +1,5 @@
+const days = document.getElementById("days");
+const hours = document.getElementById("hours");
+const minutes = document.getElementById("minutes");
+const seconds = document.getElementById("seconds");
+const countdown = document.getElementById("countdown");
